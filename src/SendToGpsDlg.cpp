9d4a4c1b2b76f53433bbd51df60302675a7a0cad
Mario Konrad <mario.konrad@gmx.net>
2013-08-16 13:30:13 +0200

    SendToGpsDlg cleanup

--- src/SendToGpsDlg.cpp
+++ src/SendToGpsDlg.cpp
@@ -19,145 +19,166 @@
  *   along with this program; if not, write to the                         *
  *   Free Software Foundation, Inc.,                                       *
  *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,  USA.         *
- ***************************************************************************
- */
-
-#include <wx/combobox.h>
+ **************************************************************************/
 
 #include "SendToGpsDlg.h"
 #include "Route.h"
 #include "RoutePoint.h"
 #include "chart1.h"
 
+#include <wx/combobox.h>
+
 extern wxString g_uploadConnection;
 
 IMPLEMENT_DYNAMIC_CLASS(SendToGpsDlg, wxDialog)
 
-BEGIN_EVENT_TABLE( SendToGpsDlg, wxDialog ) EVT_BUTTON( ID_STG_CANCEL, SendToGpsDlg::OnCancelClick )
+BEGIN_EVENT_TABLE( SendToGpsDlg, wxDialog )
+	EVT_BUTTON( ID_STG_CANCEL, SendToGpsDlg::OnCancelClick )
 	EVT_BUTTON( ID_STG_OK, SendToGpsDlg::OnSendClick )
 END_EVENT_TABLE()
 
 SendToGpsDlg::SendToGpsDlg()
 {
-    m_itemCommListBox = NULL;
-    m_pgauge = NULL;
-    m_SendButton = NULL;
-    m_CancelButton = NULL;
-    m_pRoute = NULL;
-    m_pRoutePoint = NULL;
+	m_itemCommListBox = NULL;
+	m_pgauge = NULL;
+	m_SendButton = NULL;
+	m_CancelButton = NULL;
+	m_pRoute = NULL;
+	m_pRoutePoint = NULL;
 }
 
-SendToGpsDlg::SendToGpsDlg( wxWindow* parent, wxWindowID id, const wxString& caption,
-        const wxString& hint, const wxPoint& pos, const wxSize& size, long style )
+SendToGpsDlg::SendToGpsDlg(
+		wxWindow * parent,
+		wxWindowID id,
+		const wxString & caption,
+		const wxString & hint,
+		const wxPoint & pos,
+		const wxSize & size,
+		long style)
 {
-    Create( parent, id, caption, hint, pos, size, style );
+	Create(parent, id, caption, hint, pos, size, style);
 }
 
 SendToGpsDlg::~SendToGpsDlg()
 {
-    delete m_itemCommListBox;
-    delete m_pgauge;
-    delete m_SendButton;
-    delete m_CancelButton;
+	delete m_itemCommListBox;
+	delete m_pgauge;
+	delete m_SendButton;
+	delete m_CancelButton;
 }
 
-bool SendToGpsDlg::Create( wxWindow* parent, wxWindowID id, const wxString& caption,
-        const wxString& hint, const wxPoint& pos, const wxSize& size, long style )
+bool SendToGpsDlg::Create(
+		wxWindow * parent,
+		wxWindowID id,
+		const wxString & caption,
+		const wxString & hint,
+		const wxPoint & pos,
+		const wxSize & size,
+		long style)
 {
-    SetExtraStyle( GetExtraStyle() | wxWS_EX_BLOCK_EVENTS );
-    wxDialog::Create( parent, id, caption, pos, size, style );
+	SetExtraStyle(GetExtraStyle() | wxWS_EX_BLOCK_EVENTS);
+	wxDialog::Create(parent, id, caption, pos, size, style);
 
-    CreateControls( hint );
-    GetSizer()->Fit( this );
-    GetSizer()->SetSizeHints( this );
-    Centre();
+	CreateControls(hint);
+	GetSizer()->Fit(this);
+	GetSizer()->SetSizeHints(this);
+	Centre();
 
-    return TRUE;
+	return TRUE;
 }
 
-void SendToGpsDlg::CreateControls( const wxString& hint )
+void SendToGpsDlg::CreateControls(const wxString& hint)
 {
-    SendToGpsDlg* itemDialog1 = this;
+	SendToGpsDlg* itemDialog1 = this;
 
-    wxBoxSizer* itemBoxSizer2 = new wxBoxSizer( wxVERTICAL );
-    itemDialog1->SetSizer( itemBoxSizer2 );
+	wxBoxSizer* itemBoxSizer2 = new wxBoxSizer( wxVERTICAL );
+	itemDialog1->SetSizer( itemBoxSizer2 );
 
-//      Create the ScrollBox list of available com ports in a labeled static box
-    wxStaticBox* comm_box = new wxStaticBox( this, wxID_ANY, _("GPS/Plotter Port") );
+	//      Create the ScrollBox list of available com ports in a labeled static box
+	wxStaticBox* comm_box = new wxStaticBox( this, wxID_ANY, _("GPS/Plotter Port") );
 
-    wxStaticBoxSizer* comm_box_sizer = new wxStaticBoxSizer( comm_box, wxVERTICAL );
-    itemBoxSizer2->Add( comm_box_sizer, 0, wxEXPAND | wxALL, 5 );
+	wxStaticBoxSizer* comm_box_sizer = new wxStaticBoxSizer( comm_box, wxVERTICAL );
+	itemBoxSizer2->Add( comm_box_sizer, 0, wxEXPAND | wxALL, 5 );
 
-    wxArrayString *pSerialArray = EnumerateSerialPorts();
+	wxArrayString *pSerialArray = EnumerateSerialPorts();
 
-    m_itemCommListBox = new wxComboBox( this, ID_STG_CHOICE_COMM );
+	m_itemCommListBox = new wxComboBox( this, ID_STG_CHOICE_COMM );
 
-    //    Fill in the listbox with all detected serial ports
-    for( unsigned int iPortIndex = 0; iPortIndex < pSerialArray->GetCount(); iPortIndex++ ) {
-        wxString full_port = pSerialArray->Item( iPortIndex );
-        full_port.Prepend(_T("Serial:"));
-        m_itemCommListBox->Append( full_port );
-    }
+	//    Fill in the listbox with all detected serial ports
+	for( unsigned int iPortIndex = 0; iPortIndex < pSerialArray->GetCount(); iPortIndex++ ) {
+		wxString full_port = pSerialArray->Item( iPortIndex );
+		full_port.Prepend(_T("Serial:"));
+		m_itemCommListBox->Append( full_port );
+	}
 
-    delete pSerialArray;
+	delete pSerialArray;
 
-    //    Make the proper inital selection
-    if( !g_uploadConnection.IsEmpty() )
-        m_itemCommListBox->SetValue( g_uploadConnection );
-    else
-        m_itemCommListBox->SetSelection( 0 );
+	//    Make the proper inital selection
+	if( !g_uploadConnection.IsEmpty() )
+		m_itemCommListBox->SetValue( g_uploadConnection );
+	else
+		m_itemCommListBox->SetSelection( 0 );
 
-    comm_box_sizer->Add( m_itemCommListBox, 0, wxEXPAND | wxALL, 5 );
+	comm_box_sizer->Add( m_itemCommListBox, 0, wxEXPAND | wxALL, 5 );
 
-    //    Add a reminder text box
-    itemBoxSizer2->AddSpacer( 20 );
+	//    Add a reminder text box
+	itemBoxSizer2->AddSpacer( 20 );
 
-    wxStaticText *premtext = new wxStaticText( this, -1,
-            _("Prepare GPS for Route/Waypoint upload and press Send...") );
-    itemBoxSizer2->Add( premtext, 0, wxEXPAND | wxALL, 10 );
+	wxStaticText *premtext = new wxStaticText( this, -1,
+			_("Prepare GPS for Route/Waypoint upload and press Send...") );
+	itemBoxSizer2->Add( premtext, 0, wxEXPAND | wxALL, 10 );
 
-    //    Create a progress gauge
-    wxStaticBox* prog_box = new wxStaticBox( this, wxID_ANY, _("Progress...") );
+	//    Create a progress gauge
+	wxStaticBox* prog_box = new wxStaticBox( this, wxID_ANY, _("Progress...") );
 
-    wxStaticBoxSizer* prog_box_sizer = new wxStaticBoxSizer( prog_box, wxVERTICAL );
-    itemBoxSizer2->Add( prog_box_sizer, 0, wxEXPAND | wxALL, 5 );
+	wxStaticBoxSizer* prog_box_sizer = new wxStaticBoxSizer( prog_box, wxVERTICAL );
+	itemBoxSizer2->Add( prog_box_sizer, 0, wxEXPAND | wxALL, 5 );
 
-    m_pgauge = new wxGauge( this, -1, 100 );
-    prog_box_sizer->Add( m_pgauge, 0, wxEXPAND | wxALL, 5 );
+	m_pgauge = new wxGauge( this, -1, 100 );
+	prog_box_sizer->Add( m_pgauge, 0, wxEXPAND | wxALL, 5 );
 
-    //    OK/Cancel/etc.
-    wxBoxSizer* itemBoxSizer16 = new wxBoxSizer( wxHORIZONTAL );
-    itemBoxSizer2->Add( itemBoxSizer16, 0, wxALIGN_RIGHT | wxALL, 5 );
+	//    OK/Cancel/etc.
+	wxBoxSizer* itemBoxSizer16 = new wxBoxSizer( wxHORIZONTAL );
+	itemBoxSizer2->Add( itemBoxSizer16, 0, wxALIGN_RIGHT | wxALL, 5 );
 
-    m_CancelButton = new wxButton( itemDialog1, ID_STG_CANCEL, _("Cancel"), wxDefaultPosition,
-            wxDefaultSize, 0 );
-    itemBoxSizer16->Add( m_CancelButton, 0, wxALIGN_CENTER_VERTICAL | wxALL, 5 );
+	m_CancelButton = new wxButton( itemDialog1, ID_STG_CANCEL, _("Cancel"), wxDefaultPosition,
+			wxDefaultSize, 0 );
+	itemBoxSizer16->Add( m_CancelButton, 0, wxALIGN_CENTER_VERTICAL | wxALL, 5 );
 
-    m_SendButton = new wxButton( itemDialog1, ID_STG_OK, _("Send"), wxDefaultPosition,
-            wxDefaultSize, 0 );
-    itemBoxSizer16->Add( m_SendButton, 0, wxALIGN_CENTER_VERTICAL | wxALL, 5 );
-    m_SendButton->SetDefault();
+	m_SendButton = new wxButton( itemDialog1, ID_STG_OK, _("Send"), wxDefaultPosition,
+			wxDefaultSize, 0 );
+	itemBoxSizer16->Add( m_SendButton, 0, wxALIGN_CENTER_VERTICAL | wxALL, 5 );
+	m_SendButton->SetDefault();
 
 }
 
-void SendToGpsDlg::OnSendClick( wxCommandEvent& event )
+void SendToGpsDlg::OnSendClick(wxCommandEvent& event)
 {
-    //    Get the selected comm port
-    wxString src = m_itemCommListBox->GetValue();
-    g_uploadConnection = src;                   // save for persistence
+	//    Get the selected comm port
+	wxString src = m_itemCommListBox->GetValue();
+	g_uploadConnection = src;                   // save for persistence
 
-    //    And send it out
-    if( m_pRoute ) m_pRoute->SendToGPS( src, true, m_pgauge );
-    if( m_pRoutePoint ) m_pRoutePoint->SendToGPS( src, m_pgauge );
+	//    And send it out
+	if( m_pRoute ) m_pRoute->SendToGPS( src, true, m_pgauge );
+	if( m_pRoutePoint ) m_pRoutePoint->SendToGPS( src, m_pgauge );
 
-    Show( false );
-    event.Skip();
+	Show( false );
+	event.Skip();
 }
 
-void SendToGpsDlg::OnCancelClick( wxCommandEvent& event )
+void SendToGpsDlg::OnCancelClick(wxCommandEvent& event)
 {
-    Show( false );
-    event.Skip();
+	Show( false );
+	event.Skip();
 }
 
+void SendToGpsDlg::SetRoute(Route *pRoute)
+{
+	m_pRoute = pRoute;
+}
+
+void SendToGpsDlg::SetWaypoint(RoutePoint *pRoutePoint)
+{
+	m_pRoutePoint = pRoutePoint;
+}
 
---
+++ src/SendToGpsDlg.h
@@ -0,0 +1,87 @@
+/***************************************************************************
+ *
+ * Project:  OpenCPN
+ *
+ ***************************************************************************
+ *   Copyright (C) 2010 by David S. Register                               *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,  USA.         *
+ **************************************************************************/
+
+#ifndef __SENDTOGPSDLG_H__
+#define __SENDTOGPSDLG_H__
+
+#include <wx/dialog.h>
+
+#define ID_STGDIALOG 10005
+
+enum {
+	ID_STG_CANCEL = 10000,
+	ID_STG_OK,
+	ID_STG_CHOICE_COMM
+};
+
+class wxComboBox;
+class wxGauge;
+class wxButton;
+class Route;
+class RoutePoint;
+
+/// Route "Send to GPS..." Dialog Definition
+class SendToGpsDlg : public wxDialog
+{
+		DECLARE_DYNAMIC_CLASS(SendToGpsDlg)
+		DECLARE_EVENT_TABLE()
+
+	public:
+		SendToGpsDlg();
+		SendToGpsDlg(
+				wxWindow * parent,
+				wxWindowID id,
+				const wxString & caption,
+				const wxString & hint,
+				const wxPoint & pos,
+				const wxSize & size,
+				long style);
+		virtual ~SendToGpsDlg();
+
+		bool Create(
+				wxWindow * parent,
+				wxWindowID id = ID_STGDIALOG,
+				const wxString & caption = _("Send Route To GPS"),
+				const wxString & hint = _("Send Route To GPS"),
+				const wxPoint & pos = wxDefaultPosition,
+				const wxSize & size = wxSize(500, 500),
+				long style = wxCAPTION | wxRESIZE_BORDER | wxSYSTEM_MENU | wxCLOSE_BOX);
+
+		void SetRoute(Route * pRoute);
+		void SetWaypoint(RoutePoint * pRoutePoint);
+
+	private:
+		void CreateControls(const wxString & hint);
+
+		void OnCancelClick(wxCommandEvent & event);
+		void OnSendClick(wxCommandEvent & event);
+
+		Route * m_pRoute;
+		RoutePoint * m_pRoutePoint;
+		wxComboBox * m_itemCommListBox;
+		wxGauge * m_pgauge;
+		wxButton * m_CancelButton;
+		wxButton * m_SendButton;
+};
+
+#endif

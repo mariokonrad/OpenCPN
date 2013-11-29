57191a83642a9b38bb9cd8806a9828f6c8dbc71d
Mario Konrad <mario.konrad@gmx.net>
2013-11-29 14:10:30 +0100

    Cleanup: removed unused reference to global variable.

--- src/AnnunText.cpp
+++ src/AnnunText.cpp
@@ -22,17 +22,15 @@
  **************************************************************************/
 
 #include "AnnunText.h"
-#include "FontMgr.h"
-#include "StyleManager.h"
-#include "Style.h"
-#include "Routeman.h"
-
+#include <FontMgr.h>
+#include <StyleManager.h>
+#include <Style.h>
+#include <Routeman.h>
 #include <UserColors.h>
 
 #include <wx/dcmemory.h>
 
-extern Routeman * g_pRouteMan;
-extern ocpnStyle::StyleManager * g_StyleManager;
+extern ocpnStyle::StyleManager* g_StyleManager;
 
 BEGIN_EVENT_TABLE(AnnunText, wxWindow)
 	EVT_PAINT(AnnunText::OnPaint)
@@ -48,10 +46,11 @@ AnnunText::AnnunText(
 	m_label = _T("Label");
 	m_value = _T("-----");
 
-	m_plabelFont = wxTheFontList->FindOrCreateFont( 14, wxFONTFAMILY_SWISS, wxNORMAL, wxBOLD, FALSE,
-			wxString( _T("Arial Bold") ) );
-	m_pvalueFont = wxTheFontList->FindOrCreateFont( 24, wxFONTFAMILY_DEFAULT, wxNORMAL, wxBOLD,
-			FALSE, wxString( _T("helvetica") ), wxFONTENCODING_ISO8859_1 );
+	m_plabelFont = wxTheFontList->FindOrCreateFont(14, wxFONTFAMILY_SWISS, wxNORMAL, wxBOLD, FALSE,
+												   wxString(_T("Arial Bold")));
+	m_pvalueFont
+		= wxTheFontList->FindOrCreateFont(24, wxFONTFAMILY_DEFAULT, wxNORMAL, wxBOLD, FALSE,
+										  wxString(_T("helvetica")), wxFONTENCODING_ISO8859_1);
 
 	m_LegendTextElement = LegendElement;
 	m_ValueTextElement = ValueElement;
@@ -64,56 +63,58 @@ AnnunText::~AnnunText()
 
 void AnnunText::CalculateMinSize(void)
 {
-	//    Calculate the minimum required size of the window based on text size
+	// Calculate the minimum required size of the window based on text size
 
-	int wl = 50;            // reasonable defaults?
+	int wl = 50; // reasonable defaults?
 	int hl = 20;
 	int wv = 50;
 	int hv = 20;
 
-	if( m_plabelFont ) GetTextExtent( _T("1234"), &wl, &hl, NULL, NULL, m_plabelFont );
+	if (m_plabelFont)
+		GetTextExtent(_T("1234"), &wl, &hl, NULL, NULL, m_plabelFont);
 
-	if( m_pvalueFont ) GetTextExtent( _T("123.456"), &wv, &hv, NULL, NULL, m_pvalueFont );
+	if (m_pvalueFont)
+		GetTextExtent(_T("123.456"), &wv, &hv, NULL, NULL, m_pvalueFont);
 
 	wxSize min;
 	min.x = wl + wv;
-	min.y = (int) ( ( hl + hv ) * 1.2 );
+	min.y = (int)((hl + hv) * 1.2);
 
-	SetMinSize( min );
+	SetMinSize(min);
 }
 
 void AnnunText::SetColorScheme(ColorScheme)
 {
-	ocpnStyle::Style * style = g_StyleManager->GetCurrentStyle();
-	m_pbackBrush = wxTheBrushList->FindOrCreateBrush( GetGlobalColor( _T("UBLCK") ), wxSOLID );
+	ocpnStyle::Style* style = g_StyleManager->GetCurrentStyle();
+	m_pbackBrush = wxTheBrushList->FindOrCreateBrush(GetGlobalColor(_T("UBLCK")), wxSOLID);
 
 	m_text_color = style->getConsoleFontColor();
 }
 
 void AnnunText::RefreshFonts()
 {
-	m_plabelFont = FontMgr::Get().GetFont( m_LegendTextElement );
-	m_pvalueFont = FontMgr::Get().GetFont( m_ValueTextElement );
+	m_plabelFont = FontMgr::Get().GetFont(m_LegendTextElement);
+	m_pvalueFont = FontMgr::Get().GetFont(m_ValueTextElement);
 
 	CalculateMinSize();
 }
 
-void AnnunText::SetLegendElement(const wxString &element)
+void AnnunText::SetLegendElement(const wxString& element)
 {
 	m_LegendTextElement = element;
 }
 
-void AnnunText::SetValueElement(const wxString &element)
+void AnnunText::SetValueElement(const wxString& element)
 {
 	m_ValueTextElement = element;
 }
 
-void AnnunText::SetALabel(const wxString & l)
+void AnnunText::SetALabel(const wxString& l)
 {
 	m_label = l;
 }
 
-void AnnunText::SetAValue(const wxString & v)
+void AnnunText::SetAValue(const wxString& v)
 {
 	m_value = v;
 }
@@ -129,38 +130,38 @@ void AnnunText::OnPaint(wxPaintEvent &)
 	// to avoid objectionable flashing
 	wxMemoryDC mdc;
 
-	wxBitmap m_bitmap( sx, sy, -1 );
-	mdc.SelectObject( m_bitmap );
-	mdc.SetBackground( *m_pbackBrush );
+	wxBitmap m_bitmap(sx, sy, -1);
+	mdc.SelectObject(m_bitmap);
+	mdc.SetBackground(*m_pbackBrush);
 	mdc.Clear();
 
-	const wxBitmap & background = style->getConsoleTextBackground();
+	const wxBitmap& background = style->getConsoleTextBackground();
 	if (background.IsOk())
 		mdc.DrawBitmap(background, 0, 0);
 
 	mdc.SetTextForeground(m_text_color);
 
-	if( m_plabelFont ) {
-		mdc.SetFont( *m_plabelFont );
-		if ( m_pbackBrush->GetColour() != FontMgr::Get().GetFontColor( _("Console Legend") ) )
-			mdc.SetTextForeground( FontMgr::Get().GetFontColor( _("Console Legend") ) );
-		mdc.DrawText( m_label, 5, 2 );
+	if (m_plabelFont) {
+		mdc.SetFont(*m_plabelFont);
+		if (m_pbackBrush->GetColour() != FontMgr::Get().GetFontColor(_("Console Legend")))
+			mdc.SetTextForeground(FontMgr::Get().GetFontColor(_("Console Legend")));
+		mdc.DrawText(m_label, 5, 2);
 	}
 
-	if( m_pvalueFont ) {
-		mdc.SetFont( *m_pvalueFont );
-		if ( m_pbackBrush->GetColour() != FontMgr::Get().GetFontColor( _("Console Value") ) )
-			mdc.SetTextForeground( FontMgr::Get().GetFontColor( _("Console Value") ) );
+	if (m_pvalueFont) {
+		mdc.SetFont(*m_pvalueFont);
+		if (m_pbackBrush->GetColour() != FontMgr::Get().GetFontColor(_("Console Value")))
+			mdc.SetTextForeground(FontMgr::Get().GetFontColor(_("Console Value")));
 
 		int w, h;
-		mdc.GetTextExtent( m_value, &w, &h );
+		mdc.GetTextExtent(m_value, &w, &h);
 		int cw, ch;
-		mdc.GetSize( &cw, &ch );
+		mdc.GetSize(&cw, &ch);
 
-		mdc.DrawText( m_value, cw - w - 2, ch - h - 2 );
+		mdc.DrawText(m_value, cw - w - 2, ch - h - 2);
 	}
 
-	wxPaintDC dc( this );
-	dc.Blit( 0, 0, sx, sy, &mdc, 0, 0 );
+	wxPaintDC dc(this);
+	dc.Blit(0, 0, sx, sy, &mdc, 0, 0);
 }
 

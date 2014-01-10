4588eaf2b56e0117b8c5a9e341aed3cc02b75f7f
Mario Konrad <mario.konrad@gmx.net>
2014-01-10 15:26:08 +0100

    Linux build script prepared to execute actions.

--- linuxbuild.sh
+++ linuxbuild.sh
@@ -6,37 +6,199 @@
 
 # NOTE: this script works incorrect if the paths contain spaces
 
+script_path=$(dirname `readlink -f $0`)
+script_name=$(basename `readlink -f $0`)
+
 function usage()
 {
 	echo ""
-	echo "usage: $(basename $0) [options]"
+	echo "usage: ${script_name} [options]"
 	echo ""
 	echo "Options:"
-	echo "  --help       | -h  : display this help"
-	echo "  --clean      | -c  : just cleanup"
-	echo "  --verbose    | -v  : verbose"
-	echo "  --info             : print just info"
-	echo "  --cppcheck         : runs cppcheck on the entire core"
-	echo "  --understand       : creates a database for SciTools Understand"
-	echo "  --index            : creates cscope/ctags index"
+	echo "   --help       | -h  : display this help"
+	echo "   --clean      | -c  : just cleanup"
+	echo "   --verbose    | -v  : verbose"
+	echo "   --info             : print just info"
+	echo "   --cppcheck         : runs cppcheck on the entire core"
+	echo "   --understand       : creates a database for SciTools Understand"
+	echo "   --index            : creates cscope/ctags index"
 	echo ""
 	echo "Options altering the default (prepare, build, install, no packaging, not incremental):"
-	echo "  --increment  | -i  : build incrementally (no prior clean)"
-	echo "  --release    | -r  : build release"
-	echo "  --no-bulid         : prevent building"
-	echo "  --no-install       : prevent installing"
-	echo "  --no-package       : prevent build packages"
+	echo "   --increment  | -i  : build incrementally (no prior clean)"
+	echo "   --release    | -r  : build release"
+	echo "   --no-bulid         : prevent building"
+	echo "   --no-install       : prevent installing"
+	echo "   --no-package       : prevent build packages"
 	echo ""
 	echo "Options for explicit execution:"
-	echo "  --prepare          : equivalent of 'cmake' (prepare, no build, no install, no packaging, not incremental)"
-	echo "  --make             : equivalent of 'make' (no prepare, build, no install, no packaging, incremental)"
-	echo "  --install          : equivalent of 'make install' (no prepare, no build, install, no packaging, incremental)"
-	echo "  --package          : equivalent of 'cpack' (no prepare, no build, no install, packaging, incremental)"
+	echo "   --prepare          : equivalent of 'cmake' (prepare, no build, no install, no packaging, not incremental)"
+	echo "   --make             : equivalent of 'make' (no prepare, build, no install, no packaging, incremental)"
+	echo "   --install          : equivalent of 'make install' (no prepare, no build, install, no packaging, incremental)"
+	echo "   --package          : equivalent of 'cpack' (no prepare, no build, no install, packaging, incremental)"
 	echo ""
 	echo "Options for low level control:"
-	echo "  -j cores           : specify number of cores"
+	echo "   -j cores           : specify number of cores"
 	echo ""
 }
+
+
+# gather information about the platform
+cores=`grep -i 'processor' /proc/cpuinfo | wc -l`
+
+# parse options
+
+args=$(getopt \
+	--options "hrcvij:" \
+	--longopt "help" \
+	--longopt "release" \
+	--longopt "package" \
+	--longopt "clean" \
+	--longopt "verbose" \
+	--longopt "info" \
+	--longopt "index" \
+	--longopt "increment" \
+	--longopt "no-install" \
+	--longopt "make" \
+	--longopt "prepare" \
+	--longopt "install" \
+	--longopt "cppcheck" \
+	--longopt "understand" \
+	-- "$@")
+if [ $? != 0 ] ; then
+	echo "Parameter error. abort." >&2
+	exit 1
+fi
+eval set -- "${args}"
+
+opt_cleanup=0
+opt_verbose=0
+opt_info=0
+opt_prepare=1
+opt_build=1
+opt_install=1
+opt_create_packages=0
+opt_incremental=0
+opt_cppcheck=0
+opt_understand=0
+opt_actions=()
+
+function add_action()
+{
+	opt_actions[${#opt_actions[*]}]=$1
+}
+
+while [ $# -ne 0 ] ; do
+	case "$1" in
+		--help|-h)
+			usage
+			exit 1
+			;;
+		--increment|-i)
+			opt_incremental=1
+			;;
+		--release|-r)
+			BUILD_TYPE=Release
+			;;
+		--clean|-c)
+			add_action "clean"
+			opt_cleanup=1
+			;;
+		--verbose|-v)
+			opt_verbose=1
+			;;
+		--info)
+			add_action "info"
+			opt_info=1
+			opt_verbose=1
+			;;
+		--cppcheck)
+			add_action "cppcheck"
+			opt_cppcheck=1
+			;;
+		--understand)
+			add_action "understand"
+			opt_prepare=1
+			opt_understand=1
+			;;
+		--index)
+			add_action "index"
+			exec_create_index
+			exit 0
+			;;
+		--no-build)
+			opt_build=0
+			;;
+		--no-install)
+			opt_install=0
+			;;
+		--no-package)
+			opt_create_packages=0
+			;;
+		--prepare)
+			add_action "prepare"
+			opt_prepare=1
+			opt_build=0
+			opt_install=0
+			opt_create_packages=0
+			opt_incremental=0
+			;;
+		--make)
+			add_action "make"
+			opt_prepare=0
+			opt_build=1
+			opt_install=0
+			opt_create_packages=0
+			opt_incremental=1
+			;;
+		--install)
+			add_action "install"
+			opt_prepare=0
+			opt_build=0
+			opt_install=1
+			opt_create_packages=0
+			opt_incremental=1
+			;;
+		--package)
+			add_action "package"
+			opt_prepare=0
+			opt_build=0
+			opt_install=0
+			opt_create_packages=1
+			opt_incremental=1
+			;;
+		-j)
+			cores=$2
+			shift
+			;;
+		*)
+			if [ $# -gt 1 ] ; then
+				add_action "$2"
+			fi
+			;;
+	esac
+	shift
+done
+
+# ensure defaults
+if [ ${#opt_actions[*]} -eq 0 ] ; then
+	add_action "prepare"
+	add_action "make"
+	add_action "install"
+fi
+
+# ensure lower bound of cores
+if [ ${cores} -lt 1 ] ; then
+	cores=1
+fi
+
+# set necessary variables, using default values
+SRC_DIR=${SRC_DIR:-${script_path}}
+BUILD_DIR=${BUILD_DIR:-`pwd`/build}
+INSTALL_DIR=${INSTALL_DIR:-`pwd`/install}
+DEPLOY_DIR=${DEPLOY_DIR:-`pwd`/deploy}
+BUILD_TYPE=${BUILD_TYPE:-Debug}
+CORES=${CORES:-${cores}}
+CURRENT_DIR=`pwd`
 
 function cleanup_index()
 {
@@ -63,134 +225,12 @@ function exec_create_index()
 	cscope -b
 }
 
+function exec_info()
+{
+	if [ ${opt_verbose} -eq 0 ] ; then
+		return
+	fi
 
-
-# gather information about the platform
-script_path=$(dirname `readlink -f $0`)
-cores=`grep -i 'processor' /proc/cpuinfo | wc -l`
-
-
-# parse options
-
-args=`getopt --options "hrcvij:" --longoptions help,release,package,clean,verbose,info,index,increment,no-install,make,prepare,install,cppcheck,understand -- "$@"`
-if [ $? != 0 ] ; then
-	echo "Parameter error. abort." >&2
-	exit 1
-fi
-eval set -- "${args}"
-
-opt_cleanup=0
-opt_verbose=0
-opt_info=0
-opt_prepare=1
-opt_build=1
-opt_install=1
-opt_create_packages=0
-opt_incremental=0
-opt_cppcheck=0
-opt_understand=0
-
-while true ; do
-	case "$1" in
-		--help|-h)
-			usage
-			exit 1
-			;;
-		--increment|-i)
-			opt_incremental=1
-			;;
-		--release|-r)
-			BUILD_TYPE=Release
-			;;
-		--clean|-c)
-			opt_cleanup=1
-			;;
-		--verbose|-v)
-			opt_verbose=1
-			;;
-		--info)
-			opt_info=1
-			opt_verbose=1
-			;;
-		--cppcheck)
-			opt_cppcheck=1
-			;;
-		--understand)
-			opt_prepare=1
-			opt_understand=1
-			;;
-		--index)
-			exec_create_index
-			exit 0
-			;;
-		--no-build)
-			opt_build=0
-			;;
-		--no-install)
-			opt_install=0
-			;;
-		--no-package)
-			opt_create_packages=0
-			;;
-		--prepare)
-			opt_prepare=1
-			opt_build=0
-			opt_install=0
-			opt_create_packages=0
-			opt_incremental=0
-			;;
-		--make)
-			opt_prepare=0
-			opt_build=1
-			opt_install=0
-			opt_create_packages=0
-			opt_incremental=1
-			;;
-		--install)
-			opt_prepare=0
-			opt_build=0
-			opt_install=1
-			opt_create_packages=0
-			opt_incremental=1
-			;;
-		--package)
-			opt_prepare=0
-			opt_build=0
-			opt_install=0
-			opt_create_packages=1
-			opt_incremental=1
-			;;
-		-j)
-			cores=$2
-			shift
-			;;
-		--)
-			break
-			;;
-		*)
-			echo "Internal error. abort." >&2
-			exit 1
-			;;
-	esac
-	shift
-done
-
-# ensure lower bound of cores
-if [ ${cores} -lt 1 ] ; then
-	cores=1
-fi
-
-# set necessary variables, using default values
-SRC_DIR=${SRC_DIR:-${script_path}}
-BUILD_DIR=${BUILD_DIR:-`pwd`/build}
-INSTALL_DIR=${INSTALL_DIR:-`pwd`/install}
-DEPLOY_DIR=${DEPLOY_DIR:-`pwd`/deploy}
-BUILD_TYPE=${BUILD_TYPE:-Debug}
-CORES=${CORES:-${cores}}
-CURRENT_DIR=`pwd`
-
-# print information
-if [ ${opt_verbose} -ne 0 ] ; then
 	echo ""
 	echo "INFO:"
 	echo "  SRC_DIR     = ${SRC_DIR}"
@@ -216,11 +256,7 @@ if [ ${opt_verbose} -ne 0 ] ; then
 	echo "  cppcheck       : ${opt_cppcheck}"
 	echo "  understand     : ${opt_understand}"
 	echo ""
-fi
-
-if [ ${opt_info} -ne 0 ] ; then
-	exit 0
-fi
+}
 
 
 function check_build_dir()
@@ -367,6 +403,25 @@ function exec_packaging()
 	cd ${CURRENT_DIR}
 }
 
+
+# execute actions
+for action_index in ${!opt_actions[*]} ; do
+	case ${opt_actions[${action_index}]} in
+		*) # TODO: implementation of actions
+			;;
+	esac
+done
+
+
+if [ ${opt_verbose} -ne 0 ] ; then
+	exec_info
+fi
+
+if [ ${opt_info} -ne 0 ] ; then
+	exit 0
+fi
+
+exit -1
 
 # remove all directories
 if [ ${opt_cleanup} -ne 0 ] ; then

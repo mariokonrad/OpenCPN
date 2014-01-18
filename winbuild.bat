a801d89aa96d48807140053ad8bdc27d64725420
Mario Konrad <mario.konrad@gmx.net>
2014-01-18 18:20:56 +0100

    Minor cleanup of the windows build.

--- winbuild.bat
+++ winbuild.bat
@@ -3,20 +3,36 @@
 rem builds the software in the current directory
 
 rmdir /q/s build
-
 mkdir build
-
 cd build
 
-cmake -G "Visual Studio 10" -DCMAKE_BUILD_TYPE=Debug %~dp0
-rem cmake -G "Visual Studio 11" -DCMAKE_BUILD_TYPE=Debug %~dp0
-cmake --build .
+goto debug_build_vs2010
 
+:debug_build_vs2010
+echo "BUILD: debug, visual studio 2010"
+cmake -G "Visual Studio 10" -DCMAKE_BUILD_TYPE=Debug %~dp0
+cmake --build .
 rem cpack -G NSIS -C Debug
 rem cpack -G ZIP  -C Debug
+goto end
 
-rem cmake --build . --config Release
-rem cpack -G NSIS -C Release
-rem cpack -G ZIP  -C Release
+:release_build_vs2010
+echo "BUILD: release, visual studio 2010"
+cmake -G "Visual Studio 10" -DCMAKE_BUILD_TYPE=Release %~dp0
+cmake --build . --config Release
+cpack -G NSIS -C Release
+cpack -G ZIP  -C Release
+goto end
 
+:debug_build_vs2012
+echo "BUILD: debug, visual studio 2012"
+cmake -G "Visual Studio 11" -DCMAKE_BUILD_TYPE=Debug %~dp0
+cmake --build .
+rem cpack -G NSIS -C Debug
+rem cpack -G ZIP  -C Debug
+goto end
+
+
+:end
 cd ..
+

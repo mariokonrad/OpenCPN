a835ac2a165e00c74f9e1b01881d52ce59646975
Mario Konrad <mario.konrad@gmx.net>
2014-01-29 16:20:34 +0100

    The class ValueFilter added.

---
+++ src/util/ValueFilter.cpp
@@ -0,0 +1,66 @@
+/***************************************************************************
+ *
+ * Project:  OpenCPN
+ *
+ ***************************************************************************
+ *   Copyright (C) 2010 by David S. Register                               *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,  USA.         *
+ **************************************************************************/
+
+#include "ValueFilter.h"
+#include <algorithm>
+#include <numeric>
+
+namespace util {
+
+ValueFilter::ValueFilter(size_type size)
+	: values(size)
+{
+}
+
+ValueFilter::~ValueFilter()
+{
+}
+
+ValueFilter::size_type ValueFilter::size() const
+{
+	return values.size();
+}
+
+double ValueFilter::get() const
+{
+	if (size() == 0)
+		return 0.0;
+
+	return std::accumulate(values.begin(), values.end(), 0.0) / size();
+}
+
+/// Pushes a new element into the filter.
+///
+/// The new element will always be at the last position of the container.
+/// This way the values reside within the container in chronological order.
+void ValueFilter::push(double value)
+{
+	if (size() == 0)
+		return;
+	if (size() > 1)
+		std::rotate(values.begin(), values.begin() + 1, values.end());
+	values[size() - 1] = value;
+}
+
+}
+
---
+++ src_test/util/Test_ValueFilter.cpp
@@ -0,0 +1,117 @@
+#include <gtest/gtest.h>
+#include <util/ValueFilter.h>
+
+namespace {
+
+class Test_ValueFilter : public ::testing::Test {};
+
+TEST_F(Test_ValueFilter, construction_size)
+{
+	util::ValueFilter f0(0);
+	util::ValueFilter f1(1);
+	util::ValueFilter f2(2);
+	util::ValueFilter f4(4);
+
+	EXPECT_EQ(0, f0.size());
+	EXPECT_EQ(1, f1.size());
+	EXPECT_EQ(2, f2.size());
+	EXPECT_EQ(4, f4.size());
+}
+
+TEST_F(Test_ValueFilter, initialized_get)
+{
+	util::ValueFilter f(4);
+
+	EXPECT_EQ(0.0, f.get());
+}
+
+TEST_F(Test_ValueFilter, repeated_push_const_size)
+{
+	util::ValueFilter f(4);
+
+	EXPECT_EQ(4, f.size());
+	f.push(1.0);
+	EXPECT_EQ(4, f.size());
+	f.push(1.0);
+	EXPECT_EQ(4, f.size());
+	f.push(1.0);
+	EXPECT_EQ(4, f.size());
+	f.push(1.0);
+	EXPECT_EQ(4, f.size());
+}
+
+TEST_F(Test_ValueFilter, push_same_value_get)
+{
+	util::ValueFilter f(4);
+
+	f.push(1.0);
+	f.push(1.0);
+	f.push(1.0);
+	f.push(1.0);
+	EXPECT_EQ(1.0, f.get());
+}
+
+TEST_F(Test_ValueFilter, push_same_value_repeated_get)
+{
+	util::ValueFilter f(4);
+
+	f.push(4.0);
+	EXPECT_EQ(1.0, f.get());
+	EXPECT_EQ(1.0, f.get());
+	EXPECT_EQ(1.0, f.get());
+	EXPECT_EQ(1.0, f.get());
+}
+
+TEST_F(Test_ValueFilter, push_different_value_get)
+{
+	util::ValueFilter f(4);
+
+	f.push(1.0);
+	f.push(2.0);
+	f.push(4.0);
+	f.push(8.0);
+	EXPECT_EQ(15.0 / 4.0, f.get());
+}
+
+TEST_F(Test_ValueFilter, push_different_intermediate_values)
+{
+	util::ValueFilter f(4);
+
+	EXPECT_EQ(0.0, f.get());
+	f.push(1.0);
+	EXPECT_EQ(1.0 / 4.0, f.get());
+	f.push(2.0);
+	EXPECT_EQ(3.0 / 4.0, f.get());
+	f.push(4.0);
+	EXPECT_EQ(7.0 / 4.0, f.get());
+	f.push(8.0);
+	EXPECT_EQ(15.0 / 4.0, f.get());
+}
+
+TEST_F(Test_ValueFilter, push_different_intermediate_values_repeated_get)
+{
+	util::ValueFilter f(4);
+
+	EXPECT_EQ(0.0, f.get());
+	EXPECT_EQ(0.0, f.get());
+	EXPECT_EQ(0.0, f.get());
+	f.push(1.0);
+	EXPECT_EQ(1.0 / 4.0, f.get());
+	EXPECT_EQ(1.0 / 4.0, f.get());
+	EXPECT_EQ(1.0 / 4.0, f.get());
+	f.push(2.0);
+	EXPECT_EQ(3.0 / 4.0, f.get());
+	EXPECT_EQ(3.0 / 4.0, f.get());
+	EXPECT_EQ(3.0 / 4.0, f.get());
+	f.push(4.0);
+	EXPECT_EQ(7.0 / 4.0, f.get());
+	EXPECT_EQ(7.0 / 4.0, f.get());
+	EXPECT_EQ(7.0 / 4.0, f.get());
+	f.push(8.0);
+	EXPECT_EQ(15.0 / 4.0, f.get());
+	EXPECT_EQ(15.0 / 4.0, f.get());
+	EXPECT_EQ(15.0 / 4.0, f.get());
+}
+
+}
+

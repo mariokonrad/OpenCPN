0e48e052ef7f295eb72f6ff7b0104f6403e4d576
Mario Konrad <mario.konrad@gmx.net>
2014-02-19 08:02:12 +0100

    Minor documentation updates for windows builds, also changed the order of deployment in windows build script.

--- winbuild.bat
+++ winbuild.bat
@@ -13,17 +13,17 @@ echo "BUILD: debug, visual studio 2010"
 cmake -G "Visual Studio 10" -DCMAKE_BUILD_TYPE=Debug %~dp0
 cmake --build .
 cmake --build . --target unittest
-rem cpack -G NSIS -C Debug
 rem cpack -G ZIP  -C Debug
+rem cpack -G NSIS -C Debug
 goto end
 
 :release_build_vs2010
 echo "BUILD: release, visual studio 2010"
 cmake -G "Visual Studio 10" -DCMAKE_BUILD_TYPE=Release %~dp0
-cmake --build . --target unittest
+cmake --build . --config Release --target unittest
 cmake --build . --config Release
-cpack -G NSIS -C Release
 cpack -G ZIP  -C Release
+cpack -G NSIS -C Release
 goto end
 
 :debug_build_vs2012
@@ -31,8 +31,8 @@ echo "BUILD: debug, visual studio 2012"
 cmake -G "Visual Studio 11" -DCMAKE_BUILD_TYPE=Debug %~dp0
 cmake --build .
 rem cmake --build . --target unittest
-rem cpack -G NSIS -C Debug
 rem cpack -G ZIP  -C Debug
+rem cpack -G NSIS -C Debug
 goto end
 
 

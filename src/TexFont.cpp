f3cd69324fb2bad18179b4f5820e53b52d851a46
Mario Konrad <mario.konrad@gmx.net>
2014-05-04 15:27:51 +0200

    Added missing include blockers, fixed include order, consolidation of formatting.

--- src/TexFont.cpp
+++ src/TexFont.cpp
@@ -25,220 +25,228 @@
 
 #ifdef ocpnUSE_GL
 
+#include "TexFont.h"
+
 #include <wx/wx.h>
 #include <GL/gl.h>
 #include <GL/glu.h>
 
-#include "TexFont.h"
-
-void TexFont::Build( wxFont &font, bool blur )
+void TexFont::Build(wxFont& font, bool blur)
 {
-    /* avoid rebuilding if the parameters are the same */
-    if(font == m_font && blur == m_blur)
-        return;
-    
-    m_font = font;
-    m_blur = blur;
+	// avoid rebuilding if the parameters are the same
+	if (font == m_font && blur == m_blur)
+		return;
 
-    wxBitmap bmp(256, 256);
-    wxMemoryDC dc(bmp);
+	m_font = font;
+	m_blur = blur;
 
-    dc.SetFont( font );
+	wxBitmap bmp(256, 256);
+	wxMemoryDC dc(bmp);
 
-    int maxglyphw = 0, maxglyphh = 0;
-    for( int i = MIN_GLYPH; i < MAX_GLYPH; i++ ) {
-        wxCoord gw, gh;
-        wxString text;
-        if(i == DEGREE_GLYPH)
-            text = _T("°");
-        else
-            text = wxString::Format(_T("%c"), i);
-        wxCoord descent, exlead;
-        dc.GetTextExtent( text, &gw, &gh, &descent, &exlead, &font ); // measure the text
+	dc.SetFont(font);
 
-        tgi[i].width = gw;
-        tgi[i].height = gh;
+	int maxglyphw = 0, maxglyphh = 0;
+	for (int i = MIN_GLYPH; i < MAX_GLYPH; i++) {
+		wxCoord gw, gh;
+		wxString text;
+		if (i == DEGREE_GLYPH)
+			text = _T("°");
+		else
+			text = wxString::Format(_T("%c"), i);
+		wxCoord descent, exlead;
+		dc.GetTextExtent(text, &gw, &gh, &descent, &exlead, &font); // measure the text
 
-        tgi[i].advance = gw;
+		tgi[i].width = gw;
+		tgi[i].height = gh;
 
-        maxglyphw = wxMax(gw, maxglyphw);
-        maxglyphh = wxMax(gh, maxglyphh);
-    }
+		tgi[i].advance = gw;
 
-    /* add extra pixel to give a border between rows of characters
-       without this, in some cases a faint line can be see on the edge
-       from the character above */
-    maxglyphh++;
+		maxglyphw = wxMax(gw, maxglyphw);
+		maxglyphh = wxMax(gh, maxglyphh);
+	}
 
-    int w = COLS_GLYPHS * maxglyphw;
-    int h = ROWS_GLYPHS * maxglyphh;
+	// add extra pixel to give a border between rows of characters
+	// without this, in some cases a faint line can be see on the edge
+	// from the character above
+	maxglyphh++;
 
-    wxASSERT(w < 2048 && h < 2048);
+	int w = COLS_GLYPHS * maxglyphw;
+	int h = ROWS_GLYPHS * maxglyphh;
 
-    /* make power of 2 */
-    for(tex_w = 1; tex_w < w; tex_w *= 2);
-    for(tex_h = 1; tex_h < h; tex_h *= 2);
+	wxASSERT(w < 2048 && h < 2048);
 
-    wxBitmap tbmp(tex_w, tex_h);
-    dc.SelectObject(tbmp);
+	// make power of 2
+	for (tex_w = 1; tex_w < w; tex_w *= 2)
+		;
+	for (tex_h = 1; tex_h < h; tex_h *= 2)
+		;
 
-    /* fill bitmap with black */
-    dc.SetBackground( wxBrush( wxColour( 0, 0, 0 ) ) );
-    dc.Clear();
-        
-    /* draw the text white */
-    dc.SetTextForeground( wxColour( 255, 255, 255 ) );
+	wxBitmap tbmp(tex_w, tex_h);
+	dc.SelectObject(tbmp);
 
-    int row = 0, col = 0;
-    for( int i = MIN_GLYPH; i < MAX_GLYPH; i++ ) {
-        if(col == COLS_GLYPHS) {
-            col = 0;
-            row++;
-        }
+	// fill bitmap with black
+	dc.SetBackground(wxBrush(wxColour(0, 0, 0)));
+	dc.Clear();
 
-        tgi[i].x = col * maxglyphw;
-        tgi[i].y = row * maxglyphh;
+	// draw the text white
+	dc.SetTextForeground(wxColour(255, 255, 255));
 
-        wxString text;
-        if(i == DEGREE_GLYPH)
-            text = _T("°");
-        else
-            text = wxString::Format(_T("%c"), i);
+	int row = 0, col = 0;
+	for (int i = MIN_GLYPH; i < MAX_GLYPH; i++) {
+		if (col == COLS_GLYPHS) {
+			col = 0;
+			row++;
+		}
 
-        dc.DrawText(text, tgi[i].x, tgi[i].y );
-        col++;
-    }
+		tgi[i].x = col * maxglyphw;
+		tgi[i].y = row * maxglyphh;
 
-    wxImage image = tbmp.ConvertToImage();
+		wxString text;
+		if (i == DEGREE_GLYPH)
+			text = _T("°");
+		else
+			text = wxString::Format(_T("%c"), i);
 
-    GLuint format, internalformat;
-    int stride;
+		dc.DrawText(text, tgi[i].x, tgi[i].y);
+		col++;
+	}
 
-    format = GL_ALPHA;
-    internalformat = format;
-    stride = 1;
+	wxImage image = tbmp.ConvertToImage();
 
-    if( m_blur )
-        image = image.Blur(1);
+	GLuint format, internalformat;
+	int stride;
 
-    unsigned char *imgdata = image.GetData();
-    unsigned char *teximage = (unsigned char *) malloc( stride * tex_w * tex_h );
+	format = GL_ALPHA;
+	internalformat = format;
+	stride = 1;
 
-    for( int j = 0; j < tex_w*tex_h; j++ )
-        for( int k = 0; k < stride; k++ )
-            teximage[j * stride + k] = imgdata[3*j];
+	if (m_blur)
+		image = image.Blur(1);
 
-    if(texobj)
-        Delete();
+	unsigned char* imgdata = image.GetData();
+	unsigned char* teximage = (unsigned char*)malloc(stride * tex_w * tex_h);
 
-    glGenTextures( 1, &texobj );
-    glBindTexture( GL_TEXTURE_2D, texobj );
+	for (int j = 0; j < tex_w * tex_h; j++)
+		for (int k = 0; k < stride; k++)
+			teximage[j * stride + k] = imgdata[3 * j];
 
-    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT );
-    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT );
-    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
-    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST );
+	if (texobj)
+		Delete();
 
-    glTexImage2D( GL_TEXTURE_2D, 0, internalformat, tex_w, tex_h, 0,
-                  format, GL_UNSIGNED_BYTE, teximage );
+	glGenTextures(1, &texobj);
+	glBindTexture(GL_TEXTURE_2D, texobj);
 
-    free(teximage);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, internalformat, tex_w, tex_h, 0, format, GL_UNSIGNED_BYTE,
+				 teximage);
+
+	free(teximage);
 }
 
-void TexFont::Delete( )
+void TexFont::Delete()
 {
-    glDeleteTextures(1, &texobj);
-    texobj = 0;
+	glDeleteTextures(1, &texobj);
+	texobj = 0;
 }
 
-void TexFont::GetTextExtent(const char *string, int len, int *width, int *height)
+void TexFont::GetTextExtent(const char* string, int len, int* width, int* height)
 {
-    int w=0, h=0;
+	int w = 0, h = 0;
 
-    for(int i = 0; i < len; i++ ) {
-        unsigned char c = string[i];
-        if(c == '\n') {
-            h += tgi[(int)'A'].height;
-            continue;
-        }
-        if(c == 0xc2 && (unsigned char)string[i+1] == 0xb0) {
-            c = DEGREE_GLYPH;
-            i++;
-        }
-        if( c < MIN_GLYPH || c >= MAX_GLYPH)
-            continue;
+	for (int i = 0; i < len; i++) {
+		unsigned char c = string[i];
+		if (c == '\n') {
+			h += tgi[(int)'A'].height;
+			continue;
+		}
+		if (c == 0xc2 && (unsigned char)string[i + 1] == 0xb0) {
+			c = DEGREE_GLYPH;
+			i++;
+		}
+		if (c < MIN_GLYPH || c >= MAX_GLYPH)
+			continue;
 
-        TexGlyphInfo &tgisi = tgi[c];
-        w += tgisi.advance;
-        if(tgisi.height > h)
-            h = tgisi.height;
-    }
-    if(width) *width = w;
-    if(height) *height = h;
+		TexGlyphInfo& tgisi = tgi[c];
+		w += tgisi.advance;
+		if (tgisi.height > h)
+			h = tgisi.height;
+	}
+	if (width)
+		*width = w;
+	if (height)
+		*height = h;
 }
 
-void TexFont::GetTextExtent(const wxString &string, int *width, int *height)
+void TexFont::GetTextExtent(const wxString& string, int* width, int* height)
 {
-    GetTextExtent(string.ToUTF8(), string.size(), width, height);
+	GetTextExtent(string.ToUTF8(), string.size(), width, height);
 }
 
-void TexFont::RenderGlyph( int c )
+void TexFont::RenderGlyph(int c)
 {
-    if( c < MIN_GLYPH || c >= MAX_GLYPH)
-        return;
+	if (c < MIN_GLYPH || c >= MAX_GLYPH)
+		return;
 
-    TexGlyphInfo &tgic = tgi[c];
+	TexGlyphInfo& tgic = tgi[c];
 
-    int x = tgic.x, y = tgic.y;
-    float w = tgic.width, h = tgic.height;
-    float tx1 = (float)x / tex_w;
-    float tx2 = (float)(x + w) / tex_w;
-    float ty1 = (float)y / tex_h;
-    float ty2 = (float)(y + h) / tex_h;
+	int x = tgic.x, y = tgic.y;
+	float w = tgic.width, h = tgic.height;
+	float tx1 = (float)x / tex_w;
+	float tx2 = (float)(x + w) / tex_w;
+	float ty1 = (float)y / tex_h;
+	float ty2 = (float)(y + h) / tex_h;
 
-    glBegin( GL_QUADS );
+	glBegin(GL_QUADS);
 
-    glTexCoord2f( tx1, ty1 );  glVertex2i( 0, 0 );
-    glTexCoord2f( tx2, ty1 );  glVertex2i( w, 0 );
-    glTexCoord2f( tx2, ty2 );  glVertex2i( w, h );
-    glTexCoord2f( tx1, ty2 );  glVertex2i( 0, h );
+	glTexCoord2f(tx1, ty1);
+	glVertex2i(0, 0);
+	glTexCoord2f(tx2, ty1);
+	glVertex2i(w, 0);
+	glTexCoord2f(tx2, ty2);
+	glVertex2i(w, h);
+	glTexCoord2f(tx1, ty2);
+	glVertex2i(0, h);
 
-    glEnd();
-    glTranslatef( tgic.advance, 0.0, 0.0 );
+	glEnd();
+	glTranslatef(tgic.advance, 0.0, 0.0);
 }
 
-void TexFont::RenderString( const char *string, int x, int y )
+void TexFont::RenderString(const char* string, int x, int y)
 {
-    glPushMatrix();
-    glTranslatef(x, y, 0);
+	glPushMatrix();
+	glTranslatef(x, y, 0);
 
-    glPushMatrix();
-    glBindTexture( GL_TEXTURE_2D, texobj);
+	glPushMatrix();
+	glBindTexture(GL_TEXTURE_2D, texobj);
 
-    for( int i = 0; string[i]; i++ ) {
-        if(string[i] == '\n') {
-            glPopMatrix();
-            glTranslatef(0, tgi[(int)'A'].height, 0);
-            glPushMatrix();
-            continue;
-        }
-        /* degree symbol */
-        if((unsigned char)string[i] == 0xc2 &&
-           (unsigned char)string[i+1] == 0xb0) {
-            RenderGlyph( DEGREE_GLYPH );
-            i++;
-            continue;
-        }
-        RenderGlyph( string[i] );
-    }
+	for (int i = 0; string[i]; i++) {
+		if (string[i] == '\n') {
+			glPopMatrix();
+			glTranslatef(0, tgi[(int)'A'].height, 0);
+			glPushMatrix();
+			continue;
+		}
+		// degree symbol
+		if ((unsigned char)string[i] == 0xc2 && (unsigned char)string[i + 1] == 0xb0) {
+			RenderGlyph(DEGREE_GLYPH);
+			i++;
+			continue;
+		}
+		RenderGlyph(string[i]);
+	}
 
-    glPopMatrix();
-    glPopMatrix();
+	glPopMatrix();
+	glPopMatrix();
 }
 
-void TexFont::RenderString( const wxString &string, int x, int y )
+void TexFont::RenderString(const wxString& string, int x, int y)
 {
-    RenderString((const char*)string.ToUTF8(), x, y);
+	RenderString((const char*)string.ToUTF8(), x, y);
 }
 
-#endif     //#ifdef ocpnUSE_GL
+#endif
+
--- src/TexFont.h
+++ src/TexFont.h
@@ -23,7 +23,12 @@
  *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,  USA.         *
  **************************************************************************/
 
-/* support ascii plus degree symbol for now pack font in a single texture 16x8 */
+#ifndef __TEXFONT__H__
+#define __TEXFONT__H__
+
+#include <wx/font.h>
+
+// support ascii plus degree symbol for now pack font in a single texture 16x8
 #define DEGREE_GLYPH 127
 #define MIN_GLYPH 32
 #define MAX_GLYPH 128
@@ -31,34 +36,43 @@
 #define NUM_GLYPHS (MAX_GLYPH - MIN_GLYPH)
 
 #define COLS_GLYPHS 16
-#define ROWS_GLYPHS ((NUM_GLYPHS / COLS_GLYPHS)+1)
+#define ROWS_GLYPHS ((NUM_GLYPHS / COLS_GLYPHS) + 1)
 
-struct TexGlyphInfo {
-    int x, y, width, height;
-    float advance;
+struct TexGlyphInfo
+{
+	int x;
+	int y;
+	int width;
+	int height;
+	float advance;
 };
 
-class TexFont {
+class TexFont
+{
 public:
-    TexFont() {}
+	TexFont()
+	{
+	}
 
-    void Build( wxFont &font, bool blur = false );
-    void Delete();
+	void Build(wxFont& font, bool blur = false);
+	void Delete();
 
-    void GetTextExtent( const char *string, int len, int *width, int *height);
-    void GetTextExtent( const wxString &string, int *width, int *height);
-    void RenderString( const char *string, int x=0, int y=0 );
-    void RenderString( const wxString &string, int x=0, int y=0 );
-
+	void GetTextExtent(const char* string, int len, int* width, int* height);
+	void GetTextExtent(const wxString& string, int* width, int* height);
+	void RenderString(const char* string, int x = 0, int y = 0);
+	void RenderString(const wxString& string, int x = 0, int y = 0);
 
 private:
-    void RenderGlyph( int c );
+	void RenderGlyph(int c);
 
-    wxFont m_font;
-    bool m_blur;
+	wxFont m_font;
+	bool m_blur;
 
-    TexGlyphInfo tgi[MAX_GLYPH];
+	TexGlyphInfo tgi[MAX_GLYPH];
 
-    unsigned  int texobj;
-    int tex_w, tex_h;
+	unsigned int texobj;
+	int tex_w;
+	int tex_h;
 };
+
+#endif
